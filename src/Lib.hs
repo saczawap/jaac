b1ca30082fd6163a0157246a8df443b237f0ca24
Paweł Sączawa <saczawap@gmail.com>
2018-05-27 22:48:37 +0200

    Clean Lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,39 +1,27 @@
 module Lib where
 
-import Network.Socket hiding (send, sendTo, recv, recvFrom)
-import Network.Socket.ByteString as NB
 import qualified Data.ByteString.Char8 as BS8
-import qualified Data.ByteString as BS
-import Debug.Trace
 import qualified Client as Client
 
-import Data.Int (Int64)
-import Data.Maybe
-import Data.Text (Text)
-import System.IO (hPutStrLn, stderr)
-
-import Data.Word
-import qualified Data.Text.Encoding as T
-import qualified Data.ByteString.Lazy as BL
-import Control.Monad.Trans.Class
-import Control.Monad.Trans.Except
-
-someFunc :: IO ()
-someFunc = do 
-    conn <- Client.openConnection $ Client.ServerAddress "172.17.0.2" "5672"
-    chann <- Client.openChannel conn
-    ex <- Client.declareExchange chann "ename"
-    q <- Client.declareQueue chann "nnn"
-    bq <- Client.bindQueue chann "ename" "nnn"
-    _ <- Client.publish conn "ename" "kkk" (BS8.pack "SOME")
-
-    
-    -- runExceptT $ consumeLoop sock
-    putStrLn "DUPA"
-    getLine
-    putStrLn "END"
+example :: IO ()
+example = do 
+    connection <- Client.openConnection serverAddress
+    channel <- Client.openChannel connection 
+    _ <- Client.declareExchange channel exchangeName
+    queue <- Client.declareQueue channel queueName
+    _ <- Client.bindQueue channel exchangeName queueName
+    _ <- Client.publish connection exchangeName routingKey (BS8.pack "A MESSAGE")
+    _ <- putStrLn "Starting consuming messages"
+    consumeLoop connection
 
 consumeLoop sock = (do
-    Client.Message tag content <- Client.consume sock "nnn"
+    Client.Message tag content <- Client.consume sock queueName
     putStrLn ("MSG: " ++ (show content))
     Client.ack sock tag) >> consumeLoop sock
+
+
+exchangeName = "exchangeName"
+queueName = "queueName"
+routingKey = "routingKey"
+
+serverAddress = Client.ServerAddress "172.17.0.2" "5672"
